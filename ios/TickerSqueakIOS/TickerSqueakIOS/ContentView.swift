b1a5f0238eb55514337b1ca88137c69b1d71a889
shkalash <shaikalev@gmail.com>
2025-06-24 14:59:36 +0300

    adding ios app

---
+++ ios/TickerSqueakIOS/TickerSqueakIOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TickerSqueakIOS
+//
+//  Created by Shai Kalev on 6/24/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ios/TickerSqueakIOS/TickerSqueakIOS/TickerSqueakIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  TickerSqueakIOSApp.swift
+//  TickerSqueakIOS
+//
+//  Created by Shai Kalev on 6/24/25.
+//
+
+import SwiftUI
+
+@main
+struct TickerSqueakIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
